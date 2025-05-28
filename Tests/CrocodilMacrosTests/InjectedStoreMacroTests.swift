c1e9ad038cdd9138e9ccc061a3b31fb4eb51a0d0
Serge Kazakov <kazaimazai@gmail.com>
2025-05-28 12:58:05 +0100

    tests implementation

---
+++ Tests/CrocodilMacrosTests/InjectedStoreMacroTests.swift
@@ -0,0 +1,46 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sergey Kazakov on 25/08/2024.
+//
+
+#if canImport(CrocodilMacros)
+import SwiftSyntaxMacros
+import SwiftSyntaxMacrosTestSupport
+import XCTest
+@testable import CrocodilMacros
+
+final class InjectedStoreMacroTests: XCTestCase {
+    func test_WhenDependencyEntry_DepenencyInjectionKeyGenerated() {
+        let macros = ["DependencyEntry": DependencyInjectionMacro.self]
+            
+        assertMacroExpansion(
+              """
+              extension Dependencies {
+                  @DependencyEntry var value = 10
+              }
+              """,
+              
+              expandedSource:
+                """
+                extension Dependencies {
+                    var value {
+                        get {
+                            self[_ValueKey.self]
+                        }
+                        set {
+                            self[_ValueKey.self] = newValue
+                        }
+                    }
+
+                    private enum _ValueKey: DependencyKey {
+                        nonisolated(unsafe) static var currentValue = 10
+                    }
+                }
+                """,
+              macros: macros
+        )
+    }
+}
+#endif
--- Tests/CrocodilTests/CrocodilTests.swift
+++ Tests/CrocodilTests/CrocodilTests.swift
@@ -1,12 +1,28 @@
 import XCTest
-@testable import Crocodil
-
+import Crocodil
+ 
+extension Dependencies {
+    @DependencyEntry var intValue: Int = 1
+    @DependencyEntry var anotherIntValue: Int = Dependency[\.intValue] + 1
+    @DependencyEntry var oneMoreIntValue: Int = Dependencies[\.anotherIntValue] + 1
+}
+ 
 final class CrocodilTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    @Dependency(\.intValue) var value
+    
+    func test_whenDependencyProvided_CanBeAccessedViaProperyWrapper() {
+        XCTAssertEqual(value, 1)
+    }
+    
+    func test_whenReadAndWriteToSharedStoresValue_NoDeadlockOccurs() {
+        XCTAssertEqual(Dependency[\.intValue], 1)
+        
+        Dependencies[\.intValue] = Dependencies[\.intValue] + 1
+        XCTAssertEqual(Dependencies[\.intValue], 2)
+    }
+    
+    func test_whenSettingDepenency_DepencencyUpdated() {
+        Dependencies[\.intValue] = 2
+        XCTAssertEqual(Dependencies[\.intValue], 2)
     }
 }
